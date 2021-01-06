e00acef4505cd24a2b8d3d7d87efd4c3b3be509b
self-titled <a17300038@ceti.mx>
2021-01-05 22:08:03 -0600

    Ligero cambio en el movimiento horizontal y mejorado el salto

--- Game/FinalProject/Assets/Scripts/PlayerMovement.cs
+++ Game/FinalProject/Assets/Scripts/PlayerMovement.cs
@@ -5,25 +5,27 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public float jumpForce = 5f;
+    public float jumpVelocity = 15f;
+    private Rigidbody2D rigidbody2d;
     
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
     
     void Update()
     {
         Jump();
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        transform.position += movement * Time.deltaTime * moveSpeed;        
+        transform.position += movement * Time.deltaTime * moveSpeed * 5;     
     }
 
     void Jump()
     {
         if (Input.GetButtonDown("Jump"))
         {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            //gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            rigidbody2d.velocity =  Vector2.up * jumpVelocity;
         }
     }
 }
