200855d43f3660ad9a8751d1c36f83392ad7f2c9
DarkyGum <76755914+DarkyGum@users.noreply.github.com>
2021-01-06 15:49:48 -0600

    Movimiento Fase 1
    No salta bien UnU

--- Game/FinalProject/Assets/Scripts/PlayerMovement.cs
+++ Game/FinalProject/Assets/Scripts/PlayerMovement.cs
@@ -4,28 +4,79 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float jumpVelocity = 15f;
     private Rigidbody2D rigidbody2d;
-    
+    private float moveSpeed;
+    private float moveSpeedSprint;
+    private float moveInput;
+    private bool isGrounded;    
+    public bool isJumping;
+    private float jumpTimeCounter;
+
+    public Transform FeetPos;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+    public float jumpTime;
+    public float jumpForce;
+
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
     }
     
+    void FixedUpdate(){
+        moveInput = Input.GetAxisRaw("Horizontal");
+        Move();
+        Jump();
+    }
+
     void Update()
     {
-        Jump();
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        transform.position += movement * Time.deltaTime * moveSpeed * 5;     
+        if (moveInput>0)
+        {
+            transform.eulerAngles = new Vector3(0,0,0);
+        }else if (moveInput<0)
+        {
+            transform.eulerAngles = new Vector3(0,180,0);
+        }
     }
 
     void Jump()
     {
-        if (Input.GetButtonDown("Jump"))
+        isGrounded = Physics2D.OverlapCircle(FeetPos.position, checkRadius, whatIsGround);
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
         {
-            //gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-            rigidbody2d.velocity =  Vector2.up * jumpVelocity;
+            rigidbody2d.velocity = Vector2.up * jumpForce;
+            isJumping = true;
+            jumpTimeCounter = jumpTime;
+        }   
+        if (Input.GetKey(KeyCode.Space) && isJumping == true)
+        {
+            if (jumpTimeCounter>0){
+                rigidbody2d.velocity = Vector2.up * jumpForce;
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            else{
+                isJumping = false;
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isJumping = false;
+            
+        }
+    }
+
+    void Move()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);   
+        if(Input.GetKey(KeyCode.LeftShift)){
+            //transform.position += movement * Time.deltaTime * moveSpeedSprint * 5;     
+            rigidbody2d.velocity = new Vector2(movement.x, movement.y)*6;
+
+        }else
+        {
+            //transform.position += movement * Time.deltaTime * moveSpeed * 5;   
+            rigidbody2d.velocity = new Vector2(movement.x, movement.y)*3;
         }
     }
 }
