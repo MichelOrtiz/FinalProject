4a8886258c0c353b3bb1e3a0f732436efac3031f
DarkyGum <76755914+DarkyGum@users.noreply.github.com>
2021-01-15 23:15:37 -0600

    Estamina implementada fase 1
    Ya sirve, pero baja en brguiza y sube aun más rapido,

--- Game/FinalProject/Assets/Scripts/PlayerMovement.cs
+++ Game/FinalProject/Assets/Scripts/PlayerMovement.cs
@@ -22,10 +22,15 @@ public class PlayerMovement : MonoBehaviour
     public Animator animator;
     public bool isFlying;
     public GameObject camara;
+    public int maxStamina = 100;
+    public int currentStamina;
+    public StaminaBar staminaBar;
 
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        currentStamina = maxStamina;
+        staminaBar.SetMaxStamina(maxStamina);
     }
     
     void FixedUpdate(){
@@ -64,18 +69,26 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.eulerAngles = new Vector3(0,180,0);
         }
+        if (Input.GetKeyDown(KeyCode.Minus))
+        {
+            TakeTirement(5);
+        }
+        if (!isJumping && isGrounded && !Input.GetKey(KeyCode.LeftShift))
+        {
+            RegenStamina(3);
+        }
         // animator.SetBool("Turn Left", moveInput<0 ); // Checks if the player turned left to start the turning animation
     } // tomen en cuenta el jefe final. si lo hacemos como en minecraft, va a estar muuuy difícil ganar. yes
 
     void Jump()
     {
-        Move();
         isGrounded = Physics2D.OverlapCircle(FeetPos.position, checkRadius, whatIsGround);
         if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
         {
             rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x, jumpForce);
             isJumping = true;
             jumpTimeCounter = jumpTime;
+            TakeTirement(2);
         }   
         if (Input.GetKey(KeyCode.Space) && isJumping == true)
         {
@@ -100,6 +113,7 @@ public class PlayerMovement : MonoBehaviour
         if(Input.GetKey(KeyCode.LeftShift)){
             //transform.position += movement * Time.deltaTime * moveSpeedSprint * 5;     
             rigidbody2d.velocity = new Vector2(movement.x, movement.y)*runningSpeed;
+            TakeTirement(1);
 
         }
         else // es en otro archivo
@@ -114,5 +128,28 @@ public class PlayerMovement : MonoBehaviour
         //rigidbody2d.gravityScale(isFlying? 0:26 )// no recuerdo como iba estp no puedo hacer nada, solo flotar cuando salto
         
         rigidbody2d.velocity = new Vector2(movement.x, movement.y)*walkingSpeed;
+        
+    }
+    void TakeTirement(int damage){
+        if (currentStamina>=0)
+        {
+            currentStamina -= damage;
+            staminaBar.SetStamina(currentStamina);
+        }
+        if (currentStamina<0)
+        {
+            staminaBar.SetStamina(0);
+        }
+    }
+    void RegenStamina(int regen){
+        if (currentStamina<=100)
+        {
+        currentStamina += regen;
+        staminaBar.SetStamina(currentStamina);
+        }
+        if (currentStamina>100)
+        {
+            staminaBar.SetStamina(100);
+        }
     }
 }
---
+++ Game/FinalProject/Assets/Scripts/StaminaBar.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StaminaBar : MonoBehaviour
+{
+    public Slider slider;
+
+    public void SetMaxStamina(int stamina){
+        slider.maxValue = stamina;
+        slider.value = stamina;
+    }
+
+    public void SetStamina(int stamina){
+        slider.value = stamina;
+    }
+}
