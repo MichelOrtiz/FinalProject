e63c5be31066cd64c745ee2a906a31af8ec3f230
Anette Ortiz <anette.michel.ortiz@gmail.com>
2021-02-11 21:43:35 -0600

    Dialogue v1
    Made a dialogue box. Added dialogue scripts, added a dialogue manager to Trufa. Modified Nico's script so that he could interact. Needs different tags to work .

--- Game/FinalProject/Assets/Scripts/Entities/Player/PlayerManager.cs
+++ Game/FinalProject/Assets/Scripts/Entities/Player/PlayerManager.cs
@@ -13,6 +13,57 @@ public class PlayerManager : Entity
 
     #endregion
 
+    #region Dialogue Trigger
+    public DialogueTrigger dialogue;
+    private RaycastHit2D hit;
+    private float angle;
+    private void SearchInteraction()
+        {
+            Vector2 interactPoint = transform.position;
+
+            if(transform.rotation.y == 0)
+            {
+                interactPoint.x += 5f;
+            }
+            else
+            {
+                interactPoint.x -= 5f;
+            }
+            
+            hit = Physics2D.Raycast(transform.position, interactPoint, Vector2.Distance(transform.position, interactPoint));
+            if (hit.collider != null)
+            {
+                Debug.DrawLine(transform.position, interactPoint, Color.blue, 5f);
+                DialogueTrigger chat = hit.collider.GetComponent<DialogueTrigger>();
+
+                chat.TriggerDialogue();
+                
+                dialogue = chat;
+
+                if(chat != null)
+                {
+                    SetFocus(chat);
+                    return;
+                }
+                else
+                {
+                    RemoveFocus();
+                }
+            }
+
+
+        }
+    private void SetFocus(DialogueTrigger newFocus)
+        {
+            newFocus.TriggerDialogue();
+        }
+        private void RemoveFocus()
+        {
+            dialogue = null;
+        }
+
+    #endregion
+
     #region Constant change Parameters
     public float currentStamina;
     private float moveInput; 
@@ -48,6 +99,7 @@ public class PlayerManager : Entity
     {
         DontDestroyOnLoad(gameObject);
         base.Start();
+        angle = transform.position.z;
         //walkingSpeed = AverageSpeed-3f;
         currentStamina = maxStamina;
         staminaBar.SetMaxStamina(maxStamina);
@@ -114,6 +166,12 @@ public class PlayerManager : Entity
         {
             TakeTirement(5);
         }
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            SearchInteraction(); 
+        }
+
         Timer(1,.01f,.005f);
         // animator.SetBool("Turn Left", moveInput<0 ); // Checks if the player turned left to start the turning animation
         
