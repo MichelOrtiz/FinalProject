350eff123945c602b521642f5d0e7ca908810ec8
self-titled <65133949+self-titled@users.noreply.github.com>
2021-03-20 18:45:33 -0600

    Cactus lanzador v0.5
    - Programando parte cactus lanzador:
    >> Lanza un proyectil que, cuando toca al jugador, le causa daño y aparecen de 3 a 8 cactus normales en la pantalla (no hacen daño y pueden atravesarse).
    * Por el momento, esos cactus tienen posición de aparecer fija. Cuando veamos el spawn, esto puede cambiar
    * Los cactus sólo aparecen la primera vez que el proyectil toca al jugador (no se vuelven a generar más y más)
    * Falta que oscurezca la pantalla durante 0.5seg

---
+++ Game/FinalProject/Assets/Scripts/Entities/Enemies/Cactus/Cactus.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+public class Cactus : NormalType
+{
+    new void Start()
+    {
+        base.Start();
+    }
+    new void Update()
+    {
+        base.Update();
+    }
+}
---
+++ Game/FinalProject/Assets/Scripts/Entities/Enemies/Cactus/ThrowerCactus.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using System.Collections.Generic;
+public class ThrowerCactus : Cactus, IProjectile
+{
+    [SerializeField] private Transform shotPos;
+    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private float startTimeBtwShot;
+    [SerializeField] private GameObject allucination;
+    //[SerializeField] private float numberOfAllucinations;
+    [SerializeField] private List<Transform> allucinationsPos;
+    private int numberOfAllucinations;
+    private bool allucinationsInstantiated;
+    private Projectile projectile;
+    private float timeBtwShot;
+    new void Start()
+    {
+        base.Start();
+
+    }
+    new void Update()
+    {
+        base.Update();
+    }
+
+    protected override void MainRoutine()
+    {
+        return;
+    }
+
+    protected override void ChasePlayer()
+    {
+        if (timeBtwShot <= 0)
+        {
+            ShotProjectile(shotPos, player.GetPosition());
+            timeBtwShot = startTimeBtwShot;
+        }
+        else
+        {
+            timeBtwShot -= Time.deltaTime;
+        }
+    }
+
+    protected override void Attack()
+    {
+        player.TakeTirement(damageAmount);
+    } 
+
+    public void ProjectileAttack()
+    {
+        //player.TakeTirement(projectile.damage);
+        // make screen dark for 0.5s
+        if (!allucinationsInstantiated)
+        {
+            numberOfAllucinations = RandomGenerator.NewRandom(3, 8);
+            for (int i = 0; i < numberOfAllucinations; i++)
+            {
+                Instantiate(allucination, allucinationsPos[i].position, Quaternion.identity);
+            }
+            allucinationsInstantiated = true;
+        }
+    }
+
+    public void ShotProjectile(Transform from, Vector3 to)
+    {
+        projectile = Instantiate(projectilePrefab, from.transform.position, Quaternion.identity).GetComponent<Projectile>();
+        projectile.Setup(from, to, this);
+    }
+
+}
