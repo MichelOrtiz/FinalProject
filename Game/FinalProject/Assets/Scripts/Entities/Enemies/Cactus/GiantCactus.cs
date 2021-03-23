463830ab6c53e9cc8c1289a7d25860555a02b5ed
SamuelEsSantiago <76755769+SamuelEsSantiago@users.noreply.github.com>
2021-03-23 11:20:38 -0600

    Hotfiix Cactus gigante
    cosas spawnwan en tu inventario

--- Game/FinalProject/Assets/Scripts/Entities/Enemies/Cactus/GiantCactus.cs
+++ Game/FinalProject/Assets/Scripts/Entities/Enemies/Cactus/GiantCactus.cs
@@ -2,19 +2,31 @@ using UnityEngine;
 using System.Collections.Generic;
 public class GiantCactus : Cactus
 {
+    [Header("Objetos")]
     [SerializeField]
-    private Dictionary<string, float> objectProbability = new Dictionary<string, float>
-    {
-        {"Trash", 90.5f},
-        {"Berrie", 9.5f}, 
-        {"GoldenSnitch", 0.5f}
-    };
+    private Item[] items;
+    [SerializeField]
+    private Item trash;
+    [SerializeField]
+    private Item snitch;
+    [SerializeField]
+    private Dictionary<Item, float> objectProbability;
 
 
     
     new void Start()
     {
         base.Start();
+        objectProbability = new Dictionary<Item, float>
+        {
+            {trash, 90.5f},
+            {RandomItem(), 9.5f}, 
+            {snitch, 0.5f}
+        };
+    }
+    public Item RandomItem(){
+        int random = RandomGenerator.NewRandom(0,items.Length-1);
+        return items[random];
     }
 
     new void Update()
@@ -28,8 +40,11 @@ public class GiantCactus : Cactus
         //Item item = new Item();
         List<float> probabilities = new List<float>(objectProbability.Values);
         int objectChosen = RandomGenerator.MatchedElement(probabilities);
-        Debug.Log(objectChosen);
-        Debug.Log(new List<string>(objectProbability.Keys)[objectChosen]);
+        //Debug.Log(objectChosen);
+        Item item = new List<Item>(objectProbability.Keys)[objectChosen];
+        //Debug.Log(item.name);
+        Inventory.instance.Add(item);
+        
     }
 
     protected override void MainRoutine()
--- Game/FinalProject/Assets/Scripts/Entities/Player/Abilities/DashH.cs
+++ Game/FinalProject/Assets/Scripts/Entities/Player/Abilities/DashH.cs
@@ -9,44 +9,45 @@ public class DashH : Ability
     private float timeKeyPressed;
     public float movimiento;
     public float speed;
+    bool isDashing;
+    float currentDashTime;
     Vector2 target;
     int nKeyPressed;
     public override void UseAbility()
     {
         base.UseAbility();
-            prevGravity = body.gravityScale;
-            target = new Vector2(player.GetPosition().x + movimiento, player.GetPosition().y);
-            //body.gravityScale = 0;
-            body.velocity = target * speed;
-            timeKeyPressed=0;
+        body.velocity = new Vector2(body.velocity.x, 0f);
+        body.AddForce(new Vector2(movimiento, 0f), ForceMode2D.Impulse);
+        prevGravity = body.gravityScale;
+        body.gravityScale = 0;
+        isDashing=true;
     }
 
         
         protected override void Update(){
             this.enabled = isUnlocked;
-            if(nKeyPressed>=2){
-                nKeyPressed=0;
-                timeKeyPressed=0;
-                UseAbility();
+            if(isDashing){
+                currentDashTime += Time.deltaTime;
+                if(currentDashTime >= duration){
+                    currentDashTime=0;
+                    isDashing = false;
+                    body.gravityScale = prevGravity;
+                }
             }
-            if(timeKeyPressed!=0f){
-                timeKeyPressed+=Time.deltaTime;
+            else{
+                if(Input.GetKeyDown(hotkey)){
+                    nKeyPressed++;
+                    Debug.Log("Presionado "+hotkey.ToString()+" nTimes: " + nKeyPressed);
+                }
+                if(!Input.GetKeyDown(hotkey)){
+                    nKeyPressed = 0;
+                }
+                if(nKeyPressed>=2){
+                    nKeyPressed=0;
+                    UseAbility();
+                }
             }
-            if(timeKeyPressed>=duration){
-                nKeyPressed=0;
-                timeKeyPressed=0;
-            }
-
-            if(Input.GetKeyDown(hotkey)){
-                timeKeyPressed+=Time.deltaTime;
-                nKeyPressed++;
-                Debug.Log("Intentando usar dash " + nKeyPressed);
-            }
-
-            if((Vector2)player.GetPosition() == target){
-                target = new Vector2();
-                body.gravityScale = prevGravity;
-            }
+            
         }
         
     
