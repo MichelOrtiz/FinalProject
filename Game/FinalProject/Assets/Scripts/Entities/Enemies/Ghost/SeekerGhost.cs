1b810764a06da235093410902f09b9a61b5380eb
self-titled <65133949+self-titled@users.noreply.github.com>
2021-03-23 23:19:35 -0600

    Fantasma seguidor v1
    - Programado el fantasma seguidor:
    >> Después de 30seg desde el spawn, se divide, y se vuelve a dividir cada 5 seg.
    >> Sigue a Nico cuando lo ve y puede atravesar cosas
    >> Hay un número máximo de fantasmas seguidores por escena

---
+++ Game/FinalProject/Assets/Scripts/Entities/Enemies/Ghost/SeekerGhost.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+public class SeekerGhost : Ghost
+{
+    [SerializeField] private Transform dividePos; 
+    [SerializeField] private float baseStartTimeUntilDivide;
+    [SerializeField] private float baseMainTimeUntilDivide;
+    [SerializeField] private int maxGhosts;
+
+    private bool alreadyDivided;
+    private float startTimeUntilDivide;
+    private float mainTimeUntilDivide;
+    private int ghostsDivisions;
+
+    new void Start()
+    {
+        base.Start();
+    }
+
+    new void Update()
+    {
+        ghostsDivisions = ScenesManagers.GetObjectsOfType<SeekerGhost>().Count;
+        if (ghostsDivisions == 0)
+        {
+            if (startTimeUntilDivide < baseStartTimeUntilDivide)
+            {
+                startTimeUntilDivide += Time.deltaTime;
+            }
+            else
+            {
+                Divide();
+            }
+        }
+        else
+        {
+            if (mainTimeUntilDivide < baseMainTimeUntilDivide)
+            {
+                mainTimeUntilDivide += Time.deltaTime;
+            }
+            else
+            {
+                Divide();
+                mainTimeUntilDivide = 0;
+            }
+        }
+        
+        base.Update();
+    }
+
+    protected override void MainRoutine()
+    {
+        return;
+    }
+
+    protected override void ChasePlayer()
+    {
+        rigidbody2d.position = Vector3.MoveTowards(GetPosition(), player.GetPosition(), chaseSpeed * Time.deltaTime);
+    }
+
+    protected override void Attack()
+    {
+        player.TakeTirement(damageAmount);
+    }
+
+    private void Divide()
+    {
+        if (!alreadyDivided)
+        {
+            if (ghostsDivisions < maxGhosts)
+            {
+                Instantiate(this, dividePos.position, Quaternion.identity).baseStartTimeUntilDivide = 5f;
+                alreadyDivided = true;
+                //ghostsDivisions++;
+            }
+        }
+    }
+}
