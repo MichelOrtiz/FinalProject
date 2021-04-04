69628bba933e61a3687e20e09c57929a1e5ab78d
self-titled <65133949+self-titled@users.noreply.github.com>
2021-04-03 19:18:18 -0600

    Jefe Cristal - Rayo Láser
    - Dispara un rayo láser cada cierto tiempo si Nico se encuentra a más de un número de celdas
    - El rayo tiene velocidad
    - El rayo se detiene en la posición que tuvo Nico en el momento del disparo
    - El rayo se destruye después de unos segundos

--- Game/FinalProject/Assets/Scripts/Bosses/CristalBoss/CristalBoss.cs
+++ Game/FinalProject/Assets/Scripts/Bosses/CristalBoss/CristalBoss.cs
@@ -6,12 +6,17 @@ public class CristalBoss : MonoBehaviour
 {
     [SerializeField] private GameObject cristalBarPrefab;
     [SerializeField] private GameObject cristalBarWarning;
+    
     [SerializeField] private float intervalCristalBars;
     [SerializeField] private float warningTime;
+    
+    
     private float currentTimeAfterWarning;
     private float currentTimeBeforeWarning;
+    
     private Vector2 cristalBarSpawnPosPlayer;
     private Vector2 cristalBarSpawnPosCristal;
+
     private GameObject currentWarningPlayer;
     private GameObject currentWarningCristal;
 
@@ -30,44 +35,6 @@ public class CristalBoss : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*if (intervalCristalBars - currentTime < warningTime)
-        {
-            if (player.isGrounded && currentWarning == null)
-            {
-                cristalBarSpawnPos = new Vector2(player.GetPosition().x, player.GetPosition().y -0.5f);
-                currentWarning = Instantiate(cristalBarWarning, cristalBarSpawnPos, Quaternion.identity).gameObject;
-            }
-        }*/
-        /*if (currentTimeBeforeWarning > intervalCristalBars)
-        {
-            if (currentTimeAfterWarning > intervalCristalBars + warningTime)
-            {
-                Destroy(currentWarning);
-                Instantiate(cristalBarPrefab, cristalBarSpawnPos,  Quaternion.identity);
-                currentTimeAfterWarning = 0;
-                currentTimeBeforeWarning = 0;
-            }
-            else
-            {
-                if (currentWarning == null)
-                {
-                    if (player.isGrounded)
-                    {
-                        Debug.Log("warning warning");
-                        cristalBarSpawnPos = new Vector2(player.GetPosition().x, player.GetPosition().y -0.5f);
-                        currentWarning = Instantiate(cristalBarWarning, cristalBarSpawnPos, Quaternion.identity).gameObject;
-                    }
-                }
-                else
-                {
-                    currentTimeAfterWarning += Time.deltaTime;
-                }
-            }
-        }
-        else
-        {
-            currentTimeBeforeWarning += Time.deltaTime;
-        }*/
 
         if (currentTimeBeforeWarning > intervalCristalBars)
         {
