1a5510679bdbbc66ab5ad8880940f047a2af7481
DarkyGum <76755914+DarkyGum@users.noreply.github.com>
2021-04-05 21:16:17 -0500

    Minijuegos fisicos

---
+++ Game/FinalProject/Assets/Scripts/Bosses/FungusBoss/MultiSwitch.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiSwitch : MonoBehaviour
+{
+    public int numberOfSwitch;
+    public bool jugando;
+    [SerializeField] private List<int> Orden;
+    [SerializeField] private List<bool> OrdenJugador;
+    
+    public Door door;
+    private PlayerManager player;
+    [SerializeField] private List<Switch> switches;
+
+    
+    void Start()
+    {
+        OrdenJugador = new List<bool>(Orden.Count);
+        for (int i = 0; i < Orden.Count; i++)
+        {
+            OrdenJugador.Add(false);
+        }
+        Default();
+
+        /*allDoors = ScenesManagers.GetObjectsOfType<Door>();*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!jugando)
+        {
+            jugando = Jugando();
+        }else
+        {
+            for (int i = 0; i < switches.Count; i++)
+            {
+                if (!OrdenJugador[i] && switches[i].activado )
+                {
+                    OrdenJugador[i] = true;
+                    if (i!=Orden[i])
+                    {
+                        Default();
+                    }
+                    break;
+                }
+            }            
+        }
+        if (Activados()&& !door.isOpen)
+        {
+            door.Activate();
+        }
+        
+    }
+
+    void Default(){
+        foreach (var Switch in switches)
+        {
+            Switch.activado = false;
+        }
+        for (int i = 0; i < OrdenJugador.Count; i++)
+        {
+            OrdenJugador[i] = false;
+        }
+        jugando = false;
+    }
+    bool Jugando(){
+        return switches.Exists(s=>s.activado);
+    }
+    bool Activados(){
+        foreach (var Switch in switches)
+        {
+            if (!Switch.activado)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
