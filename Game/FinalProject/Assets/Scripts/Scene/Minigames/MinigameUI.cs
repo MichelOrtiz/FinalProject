1cea215c4e13d0869fdb9b49adda6c1aaf2d2a81
Anette Ortiz <anette.michel.ortiz@gmail.com>
2021-04-30 09:34:08 -0500

    The minigames are alive
    They are only missing a couple of design stuff. But it works ;)

--- Game/FinalProject/Assets/Scripts/Scene/Minigames/MinigameUI.cs
+++ Game/FinalProject/Assets/Scripts/Scene/Minigames/MinigameUI.cs
@@ -4,17 +4,53 @@ using UnityEngine;
 
 public class MinigameUI : MonoBehaviour
 {
-    public float maxTime = 100;
+    public static MinigameUI instance;
+    public GameObject background;
+    public GameObject canvas;
+    private GameObject currentMinigame;
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(this);
+    }
+
+    public void endMinigame(){
+        currentMinigame.SetActive(false);
+        background.SetActive(false);
+        Destroy(currentMinigame);
+    }
+
+
+    public void recieveMinigame(GameObject minigame){
+        currentMinigame = Instantiate(minigame, new Vector3(0, 0, 90), Quaternion.identity);
+        currentMinigame.transform.SetParent(canvas.transform, false);
+        background.SetActive(true);
+        timerBar.SetMaxTime(maxTime);
+        StartCoroutine(TimePassing(1f, 0.05f));
+    }
+    
+    public float maxTime = 10;
     public float currentTime;
     public TimerBar timerBar;
     private bool timePassing = false;
     [SerializeField]private bool losingTime;
 
+
     // Start is called before the first frame update
     void Start()
     {
-        timerBar.SetMaxTime(maxTime);
-        StartCoroutine(TimePassing(1f, 0.05f));
+        
         
     }
 
@@ -24,21 +60,25 @@ public class MinigameUI : MonoBehaviour
         
     }
     //Change to timerBar
-    public IEnumerator TimePassing(float timeDrowned, float drown)
+    public IEnumerator TimePassing(float timePassed, float time)
     {
-        yield return new WaitForSeconds (timeDrowned);
+        yield return new WaitForSeconds (timePassed);
         if (!timePassing)
         {
             if (currentTime>0)
             {
-            currentTime -= drown;
+            currentTime -= time;
             timerBar.SetTime(currentTime);
             }
             if (currentTime<0)
             {
                 timerBar.SetTime(0);
+                endMinigame();
+                Debug.Log("el minijuego se acabo");
             }
         }
-        yield return new WaitForSeconds(timeDrowned);
+        yield return new WaitForSeconds(timePassed);
     }
 }
+
+//inventory.instance.AddMoney(int i);
