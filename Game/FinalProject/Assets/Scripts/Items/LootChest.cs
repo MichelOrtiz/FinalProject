c3edda9f3056b19358ef75f801cb46908267eef3
SamuelEsSantiago <76755769+SamuelEsSantiago@users.noreply.github.com>
2021-05-12 10:46:55 -0500

    Booty in Beach!
    +Se añadido los botines, solo se ha implementado el de la playa tho...

---
+++ Game/FinalProject/Assets/Scripts/Items/LootChest.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New LootChest", menuName = "Inventory/LootChest", order = 0)]
+public class LootChest : ScriptableObject {
+    [SerializeField] private Item[] items;
+    [SerializeField] private float[] spawnProbability;
+    public Sprite sprite;
+    [SerializeField] private int minMoney;
+    [SerializeField] private int maxMoney;
+    [SerializeField] private int minItems;
+    [SerializeField] private int maxItems;
+    [SerializeField] private GameObject prefab;
+    private Vector3 spawnPoint;
+    
+    public void Open(){
+        int numItems = RandomGenerator.NewRandom(minItems,maxItems);
+        int money = RandomGenerator.NewRandom(minMoney,maxMoney);
+        Item[] newItems = new Item[numItems]; 
+        for(int i=0;i<newItems.Length;){
+            for(int x=0;x<items.Length;x++){
+                if(Random.value > spawnProbability[x]){
+                    newItems[i] = items[x];
+                    i++;
+                    break;
+                }
+            }
+        }
+
+        Inventory.instance.AddMoney(money);
+        for(int i=0;i<newItems.Length;i++){
+            prefab.GetComponent<Inter>().SetItem(newItems[i]);
+            Instantiate(prefab,spawnPoint,Quaternion.identity);
+        }
+    }
+    public void SetSpawnPoint(Vector3 newPoint){
+        spawnPoint = newPoint;
+    }
+
+}
+
