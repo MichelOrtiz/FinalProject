ac6fa930f9eaa75113b29f6e7c3b69ae50c9fcb1
self-titled <65133949+self-titled@users.noreply.github.com>
2021-05-18 17:27:39 -0500

    CastleBoss - Rotating Bullets
    good stuff

--- Game/FinalProject/Assets/Scripts/Bosses/Castle/Scripts/CastleBRotatingBullets.cs
+++ Game/FinalProject/Assets/Scripts/Bosses/Castle/Scripts/CastleBRotatingBullets.cs
@@ -2,17 +2,117 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CastleBRotatingBullets : MonoBehaviour
+public class CastleBRotatingBullets : MonoBehaviour, IProjectile
 {
-    // Start is called before the first frame update
+
+    #region ProjectileStuff
+    [Header("Projectile Stuff")]
+    [SerializeField] private GameObject projectilePrefab;
+    private Projectile projectile;
+    //[SerializeField] private Transform shotPoint; 
+    private Vector2 shotPoint;
+    [SerializeField] private float timeBtwShot;
+    private float currentTimeBtwShot;
+
+    [SerializeField] private float burstTime;
+    private float curBurstTime;
+
+    [SerializeField] private float timeBtwBurst;
+    private float curTimeBtwBurst;
+
+    [SerializeField] private float angleBtwShots;
+    [SerializeField] private Transform center;
+    private  float centerAngle;
+    [SerializeField] private float radius;
+
+    [SerializeField] private float timeToCompleteCircle;
+    
+    private float rotationSpeed;
+    private bool isClockwise;
+    #endregion
+
+    private PlayerManager player;
+
     void Start()
     {
-        
+       rotationSpeed = (2*Mathf.PI)/timeToCompleteCircle;
+        player = PlayerManager.instance;
     }
 
     // Update is called once per frame
     void Update()
     {
+        RotateCenter();
+        if (curTimeBtwBurst > timeBtwBurst)
+        {
+            if (curBurstTime > burstTime)
+            {
+                curBurstTime = 0;
+                curTimeBtwBurst = 0;
+
+                // Invert the direction
+                isClockwise = !isClockwise;
+            }
+            else
+            {
+                if (currentTimeBtwShot > timeBtwShot)
+                {
+                    ShotProjectiles();
+                    currentTimeBtwShot = 0;
+                }
+                else
+                {
+                    currentTimeBtwShot += Time.deltaTime;
+                }
+                curBurstTime += Time.deltaTime;
+            }
+
+        }
+        else
+        {
+            curTimeBtwBurst += Time.deltaTime;
+        }
+            
+        //}
         
+
+
+    }
+
+    void RotateCenter()
+    {
+        if (isClockwise)
+        {
+            centerAngle += rotationSpeed * Time.deltaTime;
+        }
+        else
+        {
+            centerAngle -= rotationSpeed * Time.deltaTime;
+        }
+    }
+
+
+    public void ProjectileAttack()
+    {
+        player.TakeTirement(projectile.damage);
+    }
+
+    public void ShotProjectiles()
+    {
+        float angle = 0;
+        while (angle + angleBtwShots <= 360)
+        {
+            shotPoint = center.position + MathUtils.GetVectorFromAngle(angle + centerAngle);
+
+            ShotProjectile(center.position, shotPoint);
+            angle += angleBtwShots;
+        }
+    }
+
+    public void ShotProjectile(Transform from, Vector3 to){}
+    public void ShotProjectile(Vector2 from, Vector3 to)
+    {
+        projectile = Instantiate(projectilePrefab, from, Quaternion.identity).GetComponent<Projectile>();
+        projectile.Setup(from, to, this);
     }
 }
