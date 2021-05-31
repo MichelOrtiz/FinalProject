26c630f0bb8315686f47523e55036419aeb91dd3
self-titled <65133949+self-titled@users.noreply.github.com>
2021-05-31 15:08:51 -0500

    MedufBoss - WaterVolcanoes
    - Se añadió el evento OnEmmissionEnded en Spore
    etc etc

--- Game/FinalProject/Assets/Scripts/Utils/Spores/Spore.cs
+++ Game/FinalProject/Assets/Scripts/Utils/Spores/Spore.cs
@@ -7,15 +7,39 @@ public class Spore : MonoBehaviour
     [SerializeField] private float damageAmount;
     [SerializeField] private State effectOnPlayer;
     [SerializeField] private bool stopEmittingWhenParticleCollide;
+
+    #region Events
     public delegate void ParticleCollision(GameObject other);
     public event ParticleCollision ParticleCollisionHandler;
     protected virtual void InParticleCollsion(GameObject other){
         ParticleCollisionHandler?.Invoke(other);
     }
+
+    private bool onEndedCalled;
+    public delegate void EmmissionEnded(Spore sender);
+    public event EmmissionEnded EmmissionEndedHandler;
+    protected virtual void OnEmmissionEnded(Spore sender)
+    {
+        onEndedCalled = true;
+        EmmissionEndedHandler?.Invoke(sender);
+    }
+
+    #endregion
     private PlayerManager player;
-    new private ParticleSystem particleSystem; 
+    new private ParticleSystem particleSystem;
+
+
+    /// <summary>
+    /// This function is called when the object becomes enabled and active.
+    /// </summary>
+    void OnEnable()
+    {
+        onEndedCalled = false;
+    }
+
     void Start()
     {
+        
         particleSystem = GetComponent<ParticleSystem>();
         if (particleSystem.isStopped)
         {
@@ -27,8 +51,12 @@ public class Spore : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!particleSystem.isEmitting && !onEndedCalled)
+        {
+            OnEmmissionEnded(this);
+        }
     }
+
 
     /// <summary>
     /// OnParticleCollision is called when a particle hits a collider.
