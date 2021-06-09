70cd211eb0a875c192d76d29dc4ca2fdae456680
DarkyGum <76755914+DarkyGum@users.noreply.github.com>
2021-06-09 12:53:19 -0500

    puzzle 2

--- Game/FinalProject/Assets/minijuegos/Puzzle2/PiceseScript.cs
+++ Game/FinalProject/Assets/minijuegos/Puzzle2/PiceseScript.cs
@@ -8,11 +8,19 @@ public class PiceseScript : MonoBehaviour
     private Vector3 RightPosition;
     public bool InRightPosition = false;
     public bool Selected;
+    int lado = 0;
         void Start()
     {
         RightPosition = transform.position;
-        transform.position = new Vector3(Random.Range(-256f,-250f),Random.Range(6f,13f));
-        
+        if (lado == 0)
+        {
+            transform.position = new Vector3(Random.Range(220,180),Random.Range(310,120));
+            lado++;
+        }else{
+            transform.position = new Vector3(Random.Range(590,550),Random.Range(310,120));
+            lado--;
+        }
+            
     }
 
     // Update is called once per frame
