9e0aa811ec87216ebd0073a2703d528ba0d5f7d6
SamuelEsSantiago <76755769+SamuelEsSantiago@users.noreply.github.com>
2021-08-27 12:58:58 -0500

    Ahhhh, cofre UI funcional al 50%?
    Falta:
    -Mostrar descripcion de objetos
    -Cambiar de paginas
    -Tamaño cofre concuerde
    -Guardar en partida los objetos del cofre
    -Tener una instancia del cofre costante en todas las escenas
    ... T-T

--- Game/FinalProject/Assets/Scripts/Scene/Inventory/CofreUI.cs
+++ Game/FinalProject/Assets/Scripts/Scene/Inventory/CofreUI.cs
@@ -8,28 +8,27 @@ public class CofreUI : MonoBehaviour
     public static CofreUI instance;
     private void Awake() {
         if(instance!=null){
-            Debug.Log("mmm esto podria ser malo?");
+            Debug.Log("word");
             return;
         }
         instance = this;
     }
-    private Slot focusedSlot;
+    private ItemSlot focusedSlot;
     public Button nextButtonCof;
     public Button prevButtonCof;
     public Button nextButtonInv;
     public Button prevButtonInv;
-    public GameObject menuDesplegable;
-    public GameObject cofreUI;
+
     public Text description;
     public Text nametxt;
-    Cofre cofre;
-    Inventory inventory;
-    public GameObject slotsCofUI;
-    public GameObject slotsInvUI;
+    public Cofre cofre;
+    public Inventory inventory;
+    public Transform slotsCofUI;
+    public Transform slotsInvUI;
     CofreSlot[] slotsCof;
     CofreSlot[] slotsInv;
 
-    public Slot moveItemSlot;
+    public ItemSlot moveItemSlot;
     private int pageCof;
     private int pageInv;
 
@@ -42,8 +41,7 @@ public class CofreUI : MonoBehaviour
         cofre = Cofre.instance;
         inventory.onItemChangedCallBack += UpdateUI;
         cofre.onItemChangedCallBack += UpdateUI;
-        menuDesplegable.SetActive(false);
-        slotsCof = slotsInvUI.GetComponentsInChildren<CofreSlot>();
+        slotsCof = slotsCofUI.GetComponentsInChildren<CofreSlot>();
         slotsInv = slotsInvUI.GetComponentsInChildren<CofreSlot>();
         
         pageCof = 0;
@@ -52,37 +50,26 @@ public class CofreUI : MonoBehaviour
         gameObject.SetActive(false);
     }
     private void Update() {
-        if(Input.GetKeyDown(KeyCode.I)){
-            ForceCloseUI();
+        if(Input.GetKeyDown(KeyCode.I)){ //cerrar la ventana ... deberia ser con ESC pero la pausa tambien se activa con ESC
+            gameObject.SetActive(false);
         }
-    }
-    public void NextPageInv(){
-        RemoveFocusSlot();
-        pageInv += 10;
-        if(pageInv > inventory.capacidad + 10){
-            pageInv -=10;
-            return;
+        if(inventory == null || cofre == null){ //probablemente no es necesario solo soy paranoico
+            inventory = Inventory.instance;
+            cofre = Cofre.instance;
         }
-        UpdateUI();
-    }
-    public void PrevPageInv(){
-        RemoveFocusSlot();
-        pageInv -= 10;
-        if(pageInv < 0){
-            pageInv = 0;
-            return;
-        }
-        UpdateUI();
     }
     public void UpdateUI(){
-        //Inventory
+
+        //cargar inventario en UI
+        //Debug.Log("Cargando objetos en inventario-CofreUI");
         for(int i=0; i<slotsInv.Length;i++){
-            slotsInv[i].SetIndex(i+pageInv);
-            if(i+pageInv < inventory.items.Count){
-                slotsInv[i].SetItem(inventory.items.ToArray()[i+pageInv]);
-            }else{
-                slotsInv[i].ClearSlot();
-            }
+        slotsInv[i].SetIndex(i+pageInv);
+        slotsInv[i].origen = CofreSlot.Holder.Inventario;
+        if(i+pageInv < inventory.items.Count){
+            slotsInv[i].SetItem(inventory.items[i+pageInv]);
+        }else{
+            slotsInv[i].ClearSlot();
+        }
             
         }
         if(pageInv+10 < inventory.items.Count){
@@ -92,116 +79,21 @@ public class CofreUI : MonoBehaviour
             nextButtonInv.gameObject.SetActive(false);
         }
         if(pageInv!=0){
-            nextButtonInv.gameObject.SetActive(true);
+            prevButtonInv.gameObject.SetActive(true);
         }
         else{
-            nextButtonInv.gameObject.SetActive(false);
+            prevButtonInv.gameObject.SetActive(false);
         }
-        //Cofre
+
+        //cargar cofre en UI
         for(int i=0; i<slotsCof.Length;i++){
             slotsCof[i].SetIndex(i+pageCof);
+            slotsCof[i].origen = CofreSlot.Holder.Cofre;
             if(i+pageCof < cofre.savedItems.Count){
                 slotsCof[i].SetItem(cofre.savedItems[i+pageCof]);
             }else{
                 slotsCof[i].ClearSlot();
             }
-            
         }
-        if(pageCof+20 < cofre.savedItems.Count){
-            nextButtonCof.gameObject.SetActive(true);
-        }
-        else{
-            nextButtonCof.gameObject.SetActive(false);
-        }
-        if(pageCof!=0){
-            nextButtonCof.gameObject.SetActive(true);
-        }
-        else{
-            nextButtonCof.gameObject.SetActive(false);
-        }
-
-        if(focusedSlot!=null){
-            focusedSlot.icon.color = Color.yellow;
-            focusedSlot.background.color = Color.red;
-            if(focusedSlot.GetItem()!=null){
-                description.text = focusedSlot.GetItem().descripcion;
-                nametxt.text = focusedSlot.GetItem().name;
-            }
-        }else{
-            nametxt.text = "";
-            description.text = "Selecciona un objeto";
-        }
-    }
-   
-    
-    public Slot GetFocusSlot(){
-        if(focusedSlot!=null){
-            return focusedSlot;
-        }
-        else{
-            return null;
-        }
-    }
-    public void FocusSlot(Slot slot){
-        if(focusedSlot!=null){
-            focusedSlot.icon.color=Color.white;
-            focusedSlot.background.color=Color.white;
-        }
-        focusedSlot = slot;
-        UpdateUI();
-    }
-    public void RemoveFocusSlot(){
-        if(focusedSlot!=null){
-            focusedSlot.icon.color=Color.white;
-            focusedSlot.background.color=Color.white;
-            focusedSlot = null;
-        UpdateUI();
-        }
-    }
-    public void ForceCloseUI(){
-        cofreUI.SetActive(false);
-    }
-
-    public void ShowMenuDesp(){
-        menuDesplegable.SetActive(true);
-        menuDesplegable.transform.position = Input.mousePosition;
-    }
-    public void HideMenuDesp(){
-        menuDesplegable.SetActive(false);
-    }
-    public void UseButton(){
-        Item item = focusedSlot.GetItem();
-        RemoveFocusSlot();
-        item.Use();
-        HideMenuDesp();
-    }
-    public void MoveButton(){
-        HideMenuDesp();
-        if(focusedSlot!=null){
-            SetMoveSlot(focusedSlot);
-            RemoveFocusSlot();
-        }
-    }
-    public void DeleteButton(){
-        Item item = focusedSlot.GetItem();
-        RemoveFocusSlot();
-        item.RemoveFromInventory();
-        HideMenuDesp();
-    }
-    public void SetMoveSlot(Slot slot){
-        moveItemSlot = slot;
-    }
-    public void RemoveMoveItem(){
-        moveItemSlot = null;
-    }
-    public void MoveItems(Slot destination){
-        Item aux = destination.GetItem();
-        destination.SetItem(moveItemSlot.GetItem());
-        moveItemSlot.SetItem(aux);
-        RemoveMoveItem();
-    }
-   
-    public Slot GetMoveItem(){
-        return moveItemSlot;
     }
 }
