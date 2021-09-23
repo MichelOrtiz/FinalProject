fc28819bb561c225af88c8d5c9c0d35e579686c8
SamuelEsSantiago <76755769+SamuelEsSantiago@users.noreply.github.com>
2021-09-22 22:05:31 -0500

    AlgodonState
    talvez demasiado rapido...

---
+++ Game/FinalProject/Assets/Scripts/Utils/Estates/ItemStates/AlgodonState.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+[CreateAssetMenu(fileName="New State", menuName = "States/Items/Algodon")]
+public class AlgodonState : State
+{
+    [SerializeField] private float speedMultiplier;
+    [SerializeField] private float staminaPenalty;
+    private float defaultWalkingSpeed;
+    PlayerManager player;
+    Run runOverride = null;
+
+    public override void StartAffect(StatesManager newManager)
+    {
+        base.StartAffect(newManager);
+        bool isPlayer = manager.hostEntity.GetComponent<PlayerManager>() != null;
+        if(isPlayer)
+        {
+            player = manager.hostEntity.GetComponent<PlayerManager>();
+            defaultWalkingSpeed = player.walkingSpeed;
+            player.walkingSpeed *= speedMultiplier;
+            //Hacer que la habilidad correr sea mas rapida
+            foreach(Ability a in player.abilityManager.abilities){
+                if(a.abilityName == Ability.Abilities.Correr){
+                    runOverride = (Run)a;
+                    break;
+                }
+            }
+            if(runOverride != null){
+                float newSpeedMultiplier = runOverride.GetSpeedMultiplier() * speedMultiplier;
+                runOverride.SetSpeedMultiplier(newSpeedMultiplier);
+            }
+            
+        }
+        else if (manager.hostEntity.GetComponent<Enemy>() != null)
+        {
+            StopAffect();
+        }
+    }
+    public override void Affect()
+    {
+        currentTime += Time.deltaTime;
+
+        if(currentTime >= duration){
+            StopAffect();
+        }
+    }
+    public override void StopAffect()
+    {
+        base.StopAffect();
+        bool isPlayer = manager.hostEntity.GetComponent<PlayerManager>() != null;
+        if(isPlayer){
+            player.walkingSpeed = defaultWalkingSpeed;
+            //regresar la velocidad de la habilidad run a sus valores anteriores...
+            if(runOverride != null){
+                float newSpeedMultiplier = runOverride.GetSpeedMultiplier() / speedMultiplier;
+                runOverride.SetSpeedMultiplier(newSpeedMultiplier);
+            }
+            player.TakeTirement(staminaPenalty);
+        }
+    }
+}
