17389def7beb1b1e51326d73e2aea151f0416f5a
SamuelEsSantiago <76755769+SamuelEsSantiago@users.noreply.github.com>
2021-10-21 17:46:05 -0500

    LOL
    linea 42 de loadbosslevel tenia que estar comentada

--- Game/FinalProject/Assets/Scripts/UI/Habilidades/PopUpHabUI.cs
+++ Game/FinalProject/Assets/Scripts/UI/Habilidades/PopUpHabUI.cs
@@ -11,11 +11,19 @@ public class PopUpHabUI : MonoBehaviour
     [SerializeField] TextMeshProUGUI name_Ab;
     [SerializeField] TextMeshProUGUI desc_Ab;
     [SerializeField] GameObject holder;
+    [SerializeField] GameObject groupHotkey;
+    [SerializeField] TextMeshProUGUI hotkey_text;
     public void UpdateUI(Ability ability){
         this.ability = ability;
         icon.sprite = ability.iconAbility;
         name_Ab.text = ability.abilityName.ToString();
         desc_Ab.text = ability.description;
+        if(ability.hotkey != KeyCode.None){
+            hotkey_text.text = "<" + ability.hotkey.ToString() + ">";
+        }
+        else{
+            hotkey_text.text = "<Sin asignar>";
+        }
     }
     public void GoBack(){
         holder.SetActive(true);
