0ca66351c8641ea9bcb0c7394105d0507fe199cd
SamuelEsSantiago <76755769+SamuelEsSantiago@users.noreply.github.com>
2021-11-01 14:05:29 -0600

    tunes cosas

--- Game/FinalProject/Assets/Scripts/Entities/Player/PlayerManager.cs
+++ Game/FinalProject/Assets/Scripts/Entities/Player/PlayerManager.cs
@@ -508,6 +508,7 @@ public class PlayerManager : Entity
         isInDark = false;
         isInSnow = false;  
         isInIce = false;
+        statesManager.ClearAllStates();
         ResetAnimations();
     }
     #endregion
