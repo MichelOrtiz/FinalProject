9580de66806eb61e6cbf11f99455103892f66137
DarkyGum <76755914+DarkyGum@users.noreply.github.com>
2021-11-05 13:27:08 -0600

    Jeje

--- Game/FinalProject/Assets/Scripts/Scene/SceneController.cs
+++ Game/FinalProject/Assets/Scripts/Scene/SceneController.cs
@@ -43,6 +43,7 @@ public class SceneController : MonoBehaviour
         sceneTitle?.gameObject?.SetActive(false);
         prevScene = SceneManager.GetActiveScene().buildIndex;
         currentScene = scene;
+        StartCoroutine(LoadAsynchronously(partida.sceneToLoad));
     }
     public void Load(SaveFile partida){
         //LoadScene(partida.sceneToLoad);
