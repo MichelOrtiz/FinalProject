4bd3b660722d0abacc4e2b66811d244f46a723e7
SamuelEsSantiago <76755769+SamuelEsSantiago@users.noreply.github.com>
2021-11-06 12:52:47 -0600

    cambios nada importante

--- Game/FinalProject/Assets/Scripts/Scene/Inventory/HotbarUI.cs
+++ Game/FinalProject/Assets/Scripts/Scene/Inventory/HotbarUI.cs
@@ -36,94 +36,15 @@ public class HotbarUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (PlayerManager.instance.isCaptured)
+        if (!inputs.enabled)
         {
             timeKeyPressed = 0f;
             return;
         }
-        if(inputs == null){
-            inputs = PlayerManager.instance.inputs;
+        for(int i = 0; i < slotsHotbar0.Length; i++){
+            LaunchingObj(i);
         }
-        if(timeKeyPressed > 0.5){
-            GunProjectile.instance.StartAiming();
-        }
-        if(inputs.ItemHotbarUp[0] && slotsHotbar0[0].GetItem()!=null){
-            if(timeKeyPressed <= 0.5){
-                slotsHotbar0[0].UseItem();
-            }
-            if(timeKeyPressed > 0.5){
-                GunProjectile.instance.StopAiming();
-                GunProjectile.instance.ShotObject(slotsHotbar0[0].GetItem());
-                slotsHotbar0[0].RemoveItem();
-            } 
-            timeKeyPressed = 0;
-        }
-        if(inputs.ItemHotbarUp[1] && slotsHotbar0[1].GetItem()!=null){
-            if(timeKeyPressed <= 0.5){
-                slotsHotbar0[1].UseItem();
-            }
-            if(timeKeyPressed > 0.5){
-                GunProjectile.instance.ShotObject(slotsHotbar0[1].GetItem());
-                GunProjectile.instance.StopAiming();
-                slotsHotbar0[1].RemoveItem();
-            }
-            timeKeyPressed = 0;
-        }
-        if(inputs.ItemHotbarUp[2] && slotsHotbar0[2].GetItem()!=null){
-            if(timeKeyPressed <= 0.5){
-                slotsHotbar0[2].UseItem();
-            }
-            if(timeKeyPressed > 0.5){
-                GunProjectile.instance.StopAiming();
-                GunProjectile.instance.ShotObject(slotsHotbar0[2].GetItem());
-                slotsHotbar0[2].RemoveItem();
-            }
-            timeKeyPressed = 0;
-        }
-        if(inputs.ItemHotbarUp[3] && slotsHotbar0[3].GetItem()!=null){
-            if(timeKeyPressed <= 0.5){
-                slotsHotbar0[3].UseItem();
-            }
-            if(timeKeyPressed > 0.5){
-                GunProjectile.instance.ShotObject(slotsHotbar0[3].GetItem());
-                GunProjectile.instance.StopAiming();
-                slotsHotbar0[3].RemoveItem();
-            }
-            timeKeyPressed = 0;
-        }
-        if(inputs.ItemHotbarUp[4] && slotsHotbar0[4].GetItem()!=null){
-            if(timeKeyPressed <= 0.5){
-                slotsHotbar0[4].UseItem();
-            }
-            if(timeKeyPressed > 0.5){
-                GunProjectile.instance.ShotObject(slotsHotbar0[4].GetItem());
-                GunProjectile.instance.StopAiming();
-                slotsHotbar0[4].RemoveItem();
-            }
-            timeKeyPressed = 0;
-        }
-
-
-        if(inputs.ItemHotbarDown[0] && slotsHotbar0[0].GetItem()!=null){
-            //Debug.Log("timeKey1Pressed:" + timeKeyPressed);
-            timeKeyPressed += Time.deltaTime;
-        }
-        if(inputs.ItemHotbarDown[1] && slotsHotbar0[1].GetItem()!=null){
-            //Debug.Log("timeKey2Pressed:" + timeKeyPressed);
-            timeKeyPressed += Time.deltaTime;
-        }
-        if(inputs.ItemHotbarDown[2] && slotsHotbar0[2].GetItem()!=null){
-            //Debug.Log("timeKe3yPressed:" + timeKeyPressed);
-            timeKeyPressed += Time.deltaTime;
-        }
-        if(inputs.ItemHotbarDown[3] && slotsHotbar0[3].GetItem()!=null){
-            //Debug.Log("timeKey4Pressed:" + timeKeyPressed);
-            timeKeyPressed += Time.deltaTime;
-        }
-        if(inputs.ItemHotbarDown[4] && slotsHotbar0[4].GetItem()!=null){
-            //Debug.Log("timeKey5Pressed:" + timeKeyPressed);
-            timeKeyPressed += Time.deltaTime;
-        }
+        
     }
      public void UpdateHotbar0UI(){
         for(int i=0;i<slotsHotbar0.Length;i++)
@@ -131,10 +52,27 @@ public class HotbarUI : MonoBehaviour
             if(!inventory.items.Contains(slotsHotbar0[i].GetItem())){
                 slotsHotbar0[i].SetItem(null);
             }
-            else{
-
-            }
         }
     }
-    
+    public void LaunchingObj(int i){
+        bool isValidToLaunch = slotsHotbar0[i].GetItem()!=null && slotsHotbar0[i].GetItem().type == Item.ItemType.Consumible;
+        if(timeKeyPressed > 0.5){
+            GunProjectile.instance.StartAiming();
+        }
+        if(inputs.ItemHotbarUp[i] && isValidToLaunch){
+            if(timeKeyPressed <= 0.5){
+                slotsHotbar0[i].UseItem();
+            }
+            if(timeKeyPressed > 0.5){
+                GunProjectile.instance.ShotObject(slotsHotbar0[i].GetItem());
+                GunProjectile.instance.StopAiming();
+                slotsHotbar0[i].RemoveItem();
+            }
+            timeKeyPressed = 0;
+        }
+        if(inputs.ItemHotbarDown[i] && isValidToLaunch){
+            //Debug.Log("timeKey1Pressed:" + timeKeyPressed);
+            timeKeyPressed += Time.deltaTime;
+        }
+    }
 }
