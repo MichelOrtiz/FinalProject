80bfa559b2cc4cf506d3b4bd5be5aaa36657e48a
self-titled <65133949+self-titled@users.noreply.github.com>
2021-11-15 17:08:58 -0600

    >roll a dice >get broke

---
+++ Game/FinalProject/Assets/Scripts/Utils/Estates/LethalAllergyHazard.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LethalAllergyHazard : MonoBehaviour
+{
+    private ParticleSystem particles;
+    // Start is called before the first frame update
+    /*void Start()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+}
