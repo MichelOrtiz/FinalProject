170dc5fd227bd23ccdcfa6ad2d834e2f61468eae
SamuelEsSantiago <76755769+SamuelEsSantiago@users.noreply.github.com>
2021-11-15 19:53:34 -0600

    mochila y capa achivet

--- Game/FinalProject/Assets/Scripts/Items/Equipamiento/NegateStateEquipment.cs
+++ Game/FinalProject/Assets/Scripts/Items/Equipamiento/NegateStateEquipment.cs
@@ -8,16 +8,22 @@ public class NegateStateEquipment : Equipment
     [SerializeField]protected State negateThisState;
     public override void Rutina()
     {
-        player.statesManager.RemoveState(negateThisState);
+        if(negateThisState != null){
+            player.statesManager.RemoveState(negateThisState);
+        }
     }
     public override void StartEquip()
     {
         base.StartEquip();
-        player.statesManager.bannedStates.Add(negateThisState);
+        if(negateThisState != null){
+            player.statesManager.bannedStates.Add(negateThisState);
+        }
     }
     public override void EndEquip()
     {
         base.EndEquip();
-        player.statesManager.bannedStates.Remove(negateThisState);
+        if(negateThisState != null){
+            player.statesManager.bannedStates.Remove(negateThisState);
+        }
     }
 }
